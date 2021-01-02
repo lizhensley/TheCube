bc9b7985f0ed94ea4600bc38d1af1a0a520ea118
Liz Hensley <elizabethannehensley@gmail.com>
2021-01-01 23:49:49 -0500

    add files

---
+++ scripts/chart.js
@@ -0,0 +1,123 @@
+class Chart {
+
+    constructor(data) {
+        this.data = data;
+
+        this.layoutConfig = {
+            margin: {l: 0, r: 0, b: 0, t: 0, pad: 10},
+            scene: {
+                camera: {
+                    center: {x: 0, y: 0, z: 0},
+                    eye: {x: 2, y: 1, z: 1},  
+                },
+                aspectratio: {x: 1, y: 1, z: 1},
+                xaxis: {
+                    title: '',
+                    domain: [0, 10],
+                    range: [0, 10],
+                    ticktext:['', 'scrawny', '', '', '', '', '', '', '', 'beefy', ''],
+                    tickvals:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    tickangle: -20,
+                    backgroundcolor: "rgb(251, 146, 149)",
+                    gridcolor: "rgb(250,250,250)",
+                    showbackground: true,
+                    zerolinecolor: "rgb(250,250,250)"
+                },
+                yaxis: {
+                    title: '',
+                    domain: [0, 10],
+                    range: [0, 10],
+                    ticktext:['', 'kind', '', '', '', '', '', '', '', 'mean', ''],
+                    tickvals:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    tickangle: -20,
+                    backgroundcolor: "rgb(65, 206, 221)",
+                    gridcolor: "rgb(250,250,250)",
+                    showbackground: true,
+                    zerolinecolor: "rgb(250,250,250)",
+                },
+                zaxis: {
+                    title: '',
+                    domain: [0, 10],
+                    range: [0, 10],
+                    ticktext:['', 'stupid', '', '', '', '', '', '', '', 'smart', ''],
+                    tickvals:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    tickangle: -20,
+                    backgroundcolor: "rgb(159, 153, 255)",
+                    gridcolor: "rgb(250,250,250)",
+                    showbackground: true,
+                    zerolinecolor: "rgb(250,250,250)"
+                }
+            },
+        };
+
+        this.chartConfig = {
+            modeBarButtonsToRemove: ['resetCameraLastSave3d'], 
+            responsive: true,
+            displaylogo: false
+        };
+
+        this.initializeChart();
+    }
+
+    initializeChart = function() {
+        let dataX = this.data.x;
+        let dataY = this.data.y;
+        let dataZ = this.data.z;
+        let names = this.data.names;
+        let text = new Array(dataX.length);
+
+        for (let i = 0; i < dataX.length; i++) {
+            text[i] = this.getHoverText(names[i], dataX[i], dataY[i], dataZ[i]);
+        }
+
+        this.dataConfig = [{
+            opacity: 0.6,
+            type: 'scatter3d',
+            mode: 'markers',
+            hovertemplate: '%{text}',
+            text: text,
+            showlegend: false,
+            x: dataX,
+            y: dataY,
+            z: dataZ,
+        }];
+
+        Plotly.newPlot('chart', this.dataConfig, this.layoutConfig, this.chartConfig);
+    }
+    
+    getHoverText = function(name, x, y, z) {
+        let values = [x, y, z];
+        let labels = new Array(3);
+        let adjectives = ["scrawny", "beefy", "kind", "mean", "stupid", "smart"]
+    
+        for (let i = 0; i < 3; i++) {
+            let label1 = adjectives[(i * 2)];
+            let label2 = adjectives[(i * 2) + 1]
+            if (values[i] == 0) {
+                labels[i] = label1;
+            } else if (values[i] <= 2) {
+                labels[i] = "mostly " + label1;
+            } else if (values[i] < 5) {
+                labels[i] = "kinda " + label1;
+            } else if (values[i] == 5) {
+                labels[i] = "neither " + label1 + " or " + label2;
+            } else if (values[i] < 8) {
+                labels[i] = "kinda " + label2;
+            } else if (values[i] < 10) {
+                labels[i] = "mostly " + label2;
+            } else {
+                labels[i] = label2;
+            }
+        }
+        return `<b>${name}</b><br><br>• ${labels[0]}<br>• ${labels[1]}<br>• ${labels[2]}<extra></extra>`;
+    }
+}
+
+let data = {
+    names: ["a", "b", "c"],
+    x: [1, 2, 3],
+    y: [0, 5, 10],
+    z: [4, 3, 6]
+}
+
+let chart = new Chart(data)
---
+++ scripts/firebase.js
@@ -0,0 +1,15 @@
+/* Firebase Configuarion */
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD3q9ZRi24AvWbz4En5V46XCmXlTMVpbRc",
+  authDomain: "thecube-67e2a.firebaseapp.com",
+  projectId: "thecube-67e2a",
+  storageBucket: "thecube-67e2a.appspot.com",
+  messagingSenderId: "110777223260",
+  appId: "1:110777223260:web:7533e8200d53c32600fe99",
+  measurementId: "G-CFPEXZTQ8J"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
